ffa0541dd9b8ff25ecc23641ecfdd8e855e2cfbe
Jim Mendoza <jim@nexcode.dev>
2023-02-13 10:33:55 +0400

    Initial commit.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,85 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+async function searchMinecraftUUID(username) {
+	try {
+		// Use the CORS proxy to make the request
+		const response = await axios.get(
+			`https://playerdb.co/api/player/minecraft/${username}`,
+			{
+				headers: {
+					"User-Agent": "MyAppName/1.0",
+				},
+			}
+		);
+		return response.data;
+	} catch (error) {
+		console.error(error);
+		return error;
+	}
 }
 
-export default App;
+function MinecraftUUIDSearch() {
+	const [username, setUsername] = useState("");
+	const [uuid, setUUID] = useState("");
+	const [trimmedUUID, setTrimmedUUID] = useState("");
+	const [avatar, setAvatar] = useState("");
+	const [isLoading, setIsLoading] = useState(false);
+	const [error, setError] = useState(null);
+
+	async function handleSubmit(e) {
+		e.preventDefault();
+		setIsLoading(true);
+		setError(null);
+		setUUID("");
+		setTrimmedUUID("");
+		setAvatar("");
+
+		try {
+			const response = await searchMinecraftUUID(username);
+			if (!response.data) {
+				setError("Error: Unable to fetch UUID for the given username");
+				setIsLoading(false);
+				return;
+			}
+
+			const uuid = response.data.player.id; // Get full UUID.
+			const trimmedUUID = response.data.player.raw_id; // Get trimmed UUID.
+			const avatar = response.data.player.avatar; // Get player avatar.
+			setUUID(uuid);
+			setTrimmedUUID(trimmedUUID);
+			setAvatar(avatar);
+		} catch (error) {
+			setError("Error: " + error.message);
+		} finally {
+			setIsLoading(false);
+		}
+	}
+
+	return (
+		<form onSubmit={handleSubmit}>
+			<label>
+				Minecraft username:
+				<input
+					type="text"
+					value={username}
+					onChange={(e) => setUsername(e.target.value)}
+				/>
+			</label>
+			<input type="submit" value="Search" />
+			{error && <p>{error}</p>}
+			{isLoading ? (
+				<p>Loading...</p>
+			) : (
+				uuid && (
+					<p>
+						Full UUID: {uuid} &nbsp; Trimmed UUID: {trimmedUUID} &nbsp; Avatar:{" "}
+						<img src={avatar} alt="avatar" />
+					</p>
+				)
+			)}
+		</form>
+	);
+}
+
+export default MinecraftUUIDSearch;
